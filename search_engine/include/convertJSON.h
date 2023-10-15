df36a0af507a2475912023c6c4a00f1e030240f2
Andrey Egorov <89473462+Arty525@users.noreply.github.com>
2023-10-15 17:15:38 +0300

    Add files via upload

---
+++ search_engine/include/convertJSON.h
@@ -0,0 +1,37 @@
+#pragma once
+//#include "search_engine.h"
+#include "nlohmann/json.hpp"
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include "gtest/gtest.h"
+/**
+* Класс для работы с JSON-файлами
+*/
+class ConverterJSON {
+public:
+	ConverterJSON() = default;
+	/**
+	* Метод получения содержимого файлов
+	* @return Возвращает список с содержимым файлов перечисленных
+	* в config.json
+	*/
+	std::vector<std::string> GetTextDocuments();
+	/**
+	* Метод считывает поле max_responses для определения предельного
+	* количества ответов на один запрос
+	* @return
+	*/
+	int GetResponsesLimit();
+	/**
+	* Метод получения запросов из файла requests.json
+	* @return возвращает список запросов из файла requests.json
+	*/
+	std::vector<std::string> GetRequests();
+	/**
+	* Положить в файл answers.json результаты поисковых запросов
+	*/
+	void putAnswers(std::vector<std::vector<std::pair<int, float>>> answers);
+};
