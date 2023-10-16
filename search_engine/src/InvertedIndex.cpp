a70d180d9eb8717a5f80a9a1ddd8b8c74778e54c
Andrey Egorov <89473462+Arty525@users.noreply.github.com>
2023-10-16 18:57:19 +0300

    Add files via upload

--- search_engine/src/InvertedIndex.cpp
+++ search_engine/src/InvertedIndex.cpp
@@ -1,5 +1,72 @@
-#include "inverted_index.h"
+#include "InvertedIndex.h"
 
+using namespace std;
+
+void TestInvertedIndexFunctionality(
+	const vector<string>& docs,
+	const vector<string>& requests,
+	const std::vector<vector<Entry>>& expected
+) {
+	std::vector<std::vector<Entry>> result;
+	InvertedIndex idx;
+	idx.UpdateDocumentBase(docs);
+	for (auto& request : requests) {
+		std::vector<Entry> word_count = idx.GetWordCount(request);
+		result.push_back(word_count);
+	}
+	
+	for (auto& r : result) {
+		for (auto& rr : r) {
+			std::cout << "{" << rr.doc_id << ", " << rr.count << "}" << std::endl;
+		}
+	}
+
+	ASSERT_EQ(result, expected);
+}
+TEST(TestCaseInvertedIndex, TestBasic) {
+	const vector<string> docs = {
+	"london is the capital of great britain",
+	"big ben is the nickname for the Great bell of the striking clock"
+	};
+	const vector<string> requests = { "london", "the" };
+	const vector<vector<Entry>> expected = {
+		{
+			{1, 1}
+		}, 
+		{
+			{1, 1}, {2, 3}
+		}
+	};
+	TestInvertedIndexFunctionality(docs, requests, expected);
+}
+	TEST(TestCaseInvertedIndex, TestBasic2) {
+		const vector<string> docs = {
+			"milk milk milk milk water water water",
+			"milk water water",
+			"milk milk milk milk milk water water water water water",
+			"americano cappuccino"
+		};
+		const vector<string> requests = { "milk", "water", "cappuccino" };
+		const vector<vector<Entry>> expected = {
+			{
+				{1, 4}, {2, 1}, {3, 5}
+			}, {
+				{1, 3}, {2, 2}, {3, 5}
+			}, {
+				{4, 1}
+			}
+		};
+		TestInvertedIndexFunctionality(docs, requests, expected);
+	}
+	TEST(TestCaseInvertedIndex, TestInvertedIndexMissingWord) {
+		const vector<string> docs = {
+			"a b c d e f g h i j k l",
+			"statement"
+		};
+		const vector<string> requests = { "m", "statement" };
+		const vector<vector<Entry>> expected = {{}, {{2, 1}}};
+		TestInvertedIndexFunctionality(docs, requests, expected);
+	}
 
 	/**
 	* Обновить или заполнить базу документов, по которой будем совершать
@@ -21,7 +88,7 @@ void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs) {
 				}
 				else {
 					freqLock.lock();
-					freqDictionary.insert(std::pair<std::string, std::vector<Entry>>(bufer, GetWordCount(bufer)));
+					freq_dictionary.insert(std::pair<std::string, std::vector<Entry>>(bufer, GetWordCount(bufer)));
 					bufer.clear();
 					freqLock.unlock();
 				}
@@ -54,7 +121,7 @@ std::vector<Entry> InvertedIndex::GetWordCount(const std::string& word) {
 					bufer.push_back(*it);
 				}
 				if (bufer == word) {
-					list.doc_id = i;
+					list.doc_id = i + 1;
 					++list.count;
 					bufer.clear();
 				}
